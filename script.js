43dc908d22ebba62553307c9fd2c94fe3a453edd
Lily Fitz <lilymfitz@gmail.com>
2023-03-03 18:31:28 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Variable: Mobile menu button
+
+// Variable: Navigation list
+
+// When the button is clicked, we display the navigation menu
+
+// We want the button to listen for a click event
+
+//// Variable: The navigation list is/isn't visible
+
+//// When we click the button:
+////   If the menu opens, we set the variable to true (is visible).
+////   If the menu closes, we set the variable to false (isn't visible).
